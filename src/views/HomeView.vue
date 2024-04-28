d110238cb4d3f7b2470fd096673d0153320605c7
or-abdillh <hans.abdillh05@gmail.com>
2024-04-28 18:01:38 +0800

    install Heroicons

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,13 @@
 <template>
-    <h1 class="text-center text-pastel-blue">Hello</h1>
+    <h1 class="text-center text-pastel-blue">
+        Hello
+
+        <BeakerIcon class="w-6" />
+    </h1>
 </template>
+
+<script setup lang="ts">
+
+import { BeakerIcon } from '@heroicons/vue/16/solid';
+
+</script>
